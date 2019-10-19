70781c7d4fa89f1cbfa3d52b55754b1df9a16835
christianwiese <christian.wiese@freiheit.com>
2019-10-19 11:41:38 +0200

    Read game state

--- go-client/main.go
+++ go-client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net"
@@ -12,8 +13,38 @@ const (
 	pass = "awesomepassword"
 )
 
-type game struct {
+type Fleet struct {
+	Id      int   `json:"id"`
+	OwnerID int   `json:"owner_id"`
+	Origin  int   `json:"origin"`
+	Target  int   `json:"target"`
+	Ships   []int `json:"ships"`
+	Eta     int   `json:"eta"`
+}
 
+type Player struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Itsme bool   `json:"itsme"`
+}
+
+type Planet struct {
+	Id         int   `json:"id"`
+	OwnerID    int   `json:"owner_id"`
+	X          int   `json:"x"`
+	Y          int   `json:"y"`
+	Ships      []int `json:"ships"`
+	Production []int `json:"production"`
+}
+
+type Game struct {
+	GameOver  bool     `json:"game_over"`
+	Winner    int      `json:"winner"`
+	Round     int      `json:"round"`
+	MaxRounds int      `json:"max_rounds"`
+	Fleets    []Fleet  `json:"fleets"`
+	Players   []Player `json:"players"`
+	Planets   []Planet `json:"planets"`
 }
 
 func main() {
@@ -36,6 +67,16 @@ func main() {
 			fmt.Printf("could not read response from server %v", err)
 			return
 		}
-		fmt.Print("Message from server: " + message)
+		if message[0] != '{' {
+			fmt.Println(message)
+			continue
+		}
+		gameData := &Game{}
+		fmt.Println(message)
+		err = json.Unmarshal([]byte(message), gameData)
+		if err != nil {
+			fmt.Printf("could not unmarshall data %v\n", err)
+		}
+		fmt.Printf("game state: %+v", gameData)
 	}
 }
