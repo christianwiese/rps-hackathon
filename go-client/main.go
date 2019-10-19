b17a3c53b1816530f011c18f67c5d2121b4a6e7a
christianwiese <christian.wiese@freiheit.com>
2019-10-19 11:17:16 +0200

    Login logic done

--- go-client/main.go
+++ go-client/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+)
+
+const (
+	user = "cw-test1"
+	pass = "awesomepassword"
+)
+
+type game struct {
+
+}
 
 func main() {
 	fmt.Println("starting rps client")
+
+	conn, err := net.Dial("tcp", "rps.vhenne.de:6000")
+	if err != nil {
+		fmt.Printf("could not connect to server %v", err)
+		return
+	}
+	//login
+	fmt.Fprintf(conn, fmt.Sprintf("login %s %s\n", user, pass))
+	for {
+		//read
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			fmt.Printf("could not read response from server %v", err)
+			return
+		}
+		fmt.Print("Message from server: " + message)
+	}
 }
